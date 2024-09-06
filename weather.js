c8db1c16336beb1e82ee1f33baadfa2e48afdc24
c l <c l@DESKTOP-7T9A4VO>
2024-09-06 08:20:54 -0400

    y

--- weather.js
+++ weather.js
@@ -1,2 +1,25 @@
 
+function drawChart() {
+
+    // Set Data
+    const data = google.visualization.arrayToDataTable([
+      ['Price', 'Size'],
+      [50,7],[60,8],[70,8],[80,9],[90,9],
+      [100,9],[110,10],[120,11],
+      [130,14],[140,14],[150,15]
+    ]);
+    
+    // Set Options
+    const options = {
+      title: 'House Prices vs. Size',
+      hAxis: {title: 'Square Meters'},
+      vAxis: {title: 'Price in Millions'},
+      legend: 'none'
+    };
+    
+    // Draw
+    const chart = new google.visualization.LineChart(document.getElementById('myChart'));
+    chart.draw(data, options);
+    
+    }
 			
